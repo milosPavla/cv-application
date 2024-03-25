b7ebdc2b94fac025a757861595fdf4f1de43ff58
Milos Pavlovic <135544073+milosPavla@users.noreply.github.com>
2024-03-25 16:39:38 +0100

    Feature: form input handling, submission, and add reset functionality
    Extract input change handling and form submission logic into separate functions for better readability and maintainability.
    
    Update form input elements to handle changes directly within their respective form sections.
    
    Ensure form submission handlers properly prevent default form submission behavior.
    
    Add reset buttons to each form section to enable users to clear the input fields and reset the form data.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,13 +26,10 @@ export default function App() {
         companyEnd: '',
     });
 
-    function handleInputChange(e, setData) {
-        const {name, value} = e.target
-        setData(prevData => ({...prevData, [name]: value}))
-    }
-
-    function handleFormSubmit(e, setData, data) {
+    function handleFormSubmit(e, setData) {
         e.preventDefault()
+        const formData = new FormData(e.target)
+        const data = Object.fromEntries(formData.entries())
         setData(data)
     }
 
@@ -50,11 +47,12 @@ export default function App() {
                         <h4>General info</h4>
                         <a onClick={handleToggle}>▼</a>
                     </div>
-                    <form onSubmit={handleFormSubmit} className='hidden'>
-                        <input onChange={(e) => handleInputChange(e, setGeneralData)} name='name' type="text" placeholder='Your first name and last name...'/>
-                        <input onChange={(e) => handleInputChange(e, setGeneralData)} name='email' type="email" placeholder='E-mail...'/>
-                        <input onChange={(e) => handleInputChange(e, setGeneralData)} name='phone' type="text" placeholder='Phone...'/>
-                        <input onChange={(e) => handleInputChange(e, setGeneralData)} name='address' type="text" placeholder='City, Region...'/>
+                    <form onSubmit={(e) => handleFormSubmit(e, setGeneralData)} className='hidden'>
+                        <input name='name' type="text" placeholder='Your first name and last name...'/>
+                        <input name='email' type="email" placeholder='E-mail...'/>
+                        <input name='phone' type="text" placeholder='Phone...'/>
+                        <input name='address' type="text" placeholder='City, Region...'/>
+                        <button type='submit'>Submit</button>
                     </form>
                 </div>
                 <div className="educationForm">  
@@ -62,19 +60,20 @@ export default function App() {
                         <h4>Education info</h4>
                         <a onClick={handleToggle}>▼</a>
                     </div>
-                    <form onSubmit={handleFormSubmit} className='hidden'>
-                        <input onChange={(e) => handleInputChange(e, setEducationData)} name='collegeName' type="text" placeholder='College name...'/>
-                        <input onChange={(e) => handleInputChange(e, setEducationData)} name='degree' type="text" placeholder='Your degree...'/>
+                    <form onSubmit={(e) => handleFormSubmit(e, setEducationData)} className='hidden'>
+                        <input name='collegeName' type="text" placeholder='College name...'/>
+                        <input name='degree' type="text" placeholder='Your degree...'/>
                         <div className="date">
                             <div className="startDate">
                                 <p>Start date:</p>
-                                <input onChange={(e) => handleInputChange(e, setEducationData)} name='collegeStart' type="date"/>
+                                <input name='collegeStart' type="date"/>
                             </div>
                             <div className="endDate">
                                 <p>End date:</p>
-                                <input onChange={(e) => handleInputChange(e, setEducationData)} name='collegeEnd' type="date"/>
+                                <input name='collegeEnd' type="date"/>
                             </div>
                         </div>
+                        <button type='submit'>Submit</button>
                     </form>
                 </div>
                 <div className="practicalForm">  
@@ -82,19 +81,20 @@ export default function App() {
                         <h4>Practical info</h4>
                         <a onClick={handleToggle}>▼</a>
                     </div>
-                    <form onSubmit={handleFormSubmit} className='hidden'>
-                        <input onChange={(e) => handleInputChange(e, setPracticalData)} name='companyName' type="text" placeholder='Name of the company...'/>
-                        <input onChange={(e) => handleInputChange(e, setPracticalData)} name='position' type="text" placeholder='Position in company...'/>
+                    <form onSubmit={(e) => handleFormSubmit(e, setPracticalData)} className='hidden'>
+                        <input name='companyName' type="text" placeholder='Name of the company...'/>
+                        <input name='position' type="text" placeholder='Position in company...'/>
                         <div className="date">
                             <div className="startDate">
                                 <p>Start date:</p>
-                                <input onChange={(e) => handleInputChange(e, setPracticalData)} name='companyStart' type="date" placeholder='Start date...'/>
+                                <input name='companyStart' type="date" placeholder='Start date...'/>
                             </div>
                             <div className="endDate">
                                 <p>End date:</p>
-                                <input onChange={(e) => handleInputChange(e, setPracticalData)} name='companyEnd' type="date" placeholder='End date...'/>
+                                <input name='companyEnd' type="date" placeholder='End date...'/>
                             </div>
                         </div>
+                        <button type='submit'>Submit</button>
                     </form>
                 </div>
                 <p className='contact'>© Visit me on GitHub    <a href="https://github.com/milosPavla"><img src={profileImg}  /></a></p>
@@ -112,6 +112,7 @@ export default function App() {
                             <li key={key}> #<i>{key}:</i> <strong>{value}</strong></li>
                         ))}
                     </ul>  
+                    <button className='btnReset' onClick={(e) => (e.preventDefault(), setGeneralData({name: '', email: '',phone: '',address: ''}))}>Reset</button> 
                 </div>
                 <div className="educationInfo">
                 <div className="header">
@@ -122,7 +123,8 @@ export default function App() {
                         {Object.entries(educationData).map(([key, value]) => (
                             <li key={key}> #<i>{key}:</i> <strong>{value}</strong></li>
                         ))}
-                    </ul>     
+                    </ul>   
+                    <button className='btnReset' onClick={(e) => (e.preventDefault(), setEducationData({collegeName: '',degree: '',collegeStart: '',collegeEnd: '',}))}>Reset</button>   
                 </div>
                 <div className="practicalInfo">
                 <div className="header">
@@ -134,7 +136,9 @@ export default function App() {
                             <li key={key}> #<i>{key}:</i> <strong>{value}</strong></li>
                         ))}
                     </ul>    
-                </div>      
+                    <button className='btnReset' onClick={(e) => (e.preventDefault(), setPracticalData({companyName: '',position: '',companyStart: '',companyEnd: '',}))}>Reset</button> 
+                </div>    
+                 
             </div>
         </div>
     )
